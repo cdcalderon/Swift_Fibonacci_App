0a0b30b1c082bbccaf140506eeb0c26f37b9db98
carlos calderon <cdcalderon@gmail.com>
2014-11-16 19:59:56 -0600

    initial commit

--- FibonacciSeqApp/ViewController.swift
+++ FibonacciSeqApp/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var includeZeroSwitch: UISwitch!
+    @IBOutlet weak var includesZeroLabel: UILabel!
+    @IBOutlet weak var maxNumberSequenceSlider: UISlider!
 
+    @IBOutlet weak var fibonacciTextView: UITextView!
+    var fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: 2, includesZero: true)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateFibonacciSequence(){
+        if includeZeroSwitch.on {
+            maxNumberSequenceSlider.maximumValue = 94
+        }else{
+            maxNumberSequenceSlider.maximumValue = 93
+        }
+        println(maxNumberSequenceSlider.value)
+        fibonacciSequence = FibonacciSequence(numberOfItemsInSequence: UInt(maxNumberSequenceSlider.value), includesZero: includeZeroSwitch.on)
+        println(fibonacciSequence.values.description)
+        fibonacciTextView.text = fibonacciSequence.values.description
+        
+        includesZeroLabel.text = includeZeroSwitch.on ? "YES" : "NO"
+    }
 
 }
 
---
+++ FibonacciSequence.swift
@@ -0,0 +1,9 @@
+//
+//  FibonacciSequence.swift
+//  FibonacciSeqApp
+//
+//  Created by Carlos Calderon on 11/16/14.
+//  Copyright (c) 2014 Calderware. All rights reserved.
+//
+
+import Foundation
